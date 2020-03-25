61ed09b4cf69b6a1e578377104c2ed58b76fed7f
a7590370 <61215959+a7590370@users.noreply.github.com>
2020-03-25 15:29:19 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,9 +1,19 @@
-#!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+＃！/ bin / bash
+＃修改默认IP
+＃ SED -i的/ 192.168.1.1 / 192.168.50.5 / G'包/基层-文件/文件/ bin中/ config_generate
+＃修改版本号
+SED -i ' S /的OpenWrt /滨的AutoBuild $（日期“+％Y.％米。％d”）@的OpenWrt /克'包/贫/默认的设置/文件/ ZZZ-默认的设置
+＃可移除不可用    
+rm -rf包/ lean / luci-app-dockerman
+＃添加额外预算
+git clone https://github.com/tty228/luci-app-serverchan.git包/ luci-app-serverchan
+git clone https://github.com/KFERMercer/luci-app-dockerman.git包/ luci-app-dockerman
+git clone https://github.com/bin20088/luci-app-koolddns.git包/ luci-app-koolddns
+git clone https://github.com/bin20088/luci-theme-butongwifi.git包/ luci-theme-butongwifi
+＃添加神秘代码
+svn co https://github.com/bin20088/lede/trunk/files
+./scripts/feeds更新-a
+./scripts/feeds install -a
+＃授予koolddns权限
+chmod 0755软件包/luci-app-koolddns/root/etc/init.d/koolddns
+chmod 0755软件包/ luci-app-koolddns / root / usr / share / koolddns / aliddns
